aa5cc3a27b06b3037c5b2e9c0a5936636388080d
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2022-10-04 18:47:45 +0900

    [add]jissyu1

--- kiso-soft/jissyu1/sub.s
+++ kiso-soft/jissyu1/sub.s
@@ -3,17 +3,17 @@
 /*     正しくプログラムが動くように修正せよ */
 
 .section .text
-start	
+start:	
 	move.w	#9,x	/* x番地に9を格納 */
 	move.w	#4,y	/* y番地に4を格納 */
 	move.w	x, %d0  /* c番地の値をレジス夕d0に格納 */
 	sub.w	y, %d0  /* y番地の値をレジスタd0から引く */
-	move.w	%do,z	/* レジスタd0の値をz番地に格納 */  
+	move.w	%d0,z	/* レジスタd0の値をz番地に格納 */  
 	stop	#0x2700	/* 終了 */
 
 .section .data
-x:	.ds,w	1	
-y:	.ds.w	1
+x:	.ds.w   1	
+y:	.ds.w   1
 z:	.ds.w	1
 
 .end
