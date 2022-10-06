b222b6c66aecd9e4607de3869a337bd5780fa0cc
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2022-10-06 17:07:15 +0900

    [add]jissyu2, jissyu3

---
+++ kiso-soft/jissyu3/fact2.s
@@ -0,0 +1,14 @@
+/* sample program fact.s 階乗  d1 := d0 ! (d0 >= 0)*/
+.section .text
+start:
+	moveq  #1,%d1
+	cmp    #0,%d0
+	beq    end_of_program
+	move.l %d0,%d2
+loop:
+	mulu %d2,%d1
+	subq      #1,%d2
+	cmp.w  #0,%d2
+	bgt    loop
+end_of_program:
+	stop      #0x2700 /* 終了*/
