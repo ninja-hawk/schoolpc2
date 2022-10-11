bcebfe90d6a45c8277646905d1516ab2fb7c9bef
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2022-10-11 17:47:05 +0900

    [add]jissyu3完成

--- kiso-soft/jissyu3/fact3.s
+++ kiso-soft/jissyu3/fact3.s
@@ -3,19 +3,19 @@
 start:
 	clr.l  %d0 /* d0初期化 */
 	clr.l  %d2 /* d2初期化 */
+	cmp    #0, %d1
+	beq    end_of_program /* d1がゼロなら終了 */
 loop:
-	add.l %d2, %d0 /* d2をd0に */
-	cmp   #0, %d1
-	beq   end_of_program /* d1がゼロなら終了 */
-	move  %d1, %d2 /* d2乗算一時保存 */
-	move  %d1, %d3 /* d3べき乗のカウンター */
-	subq  #1,  %d3
+	move   #1, %d2 /* d2乗算一時保存 */
+	move   %d1, %d3 /* d3べき乗のカウンター */
 power:
-	mulu   %d1, %d2
-	cmp    #0, %d3
-	beq    loop
-	subi   #1, %d3
-	bra    power		
+	mulu   %d1, %d2 /* d2にd1をかける */
+	subi   #1, %d3 /* d3から1引く */
+	bne    power /* d3が0になればべき乗終了 */
+	add   %d2, %d0 /* d0にd2を入れる*/
+	subi   #1, %d1 /* d1から1引く */
+	beq    end_of_program /* d1がゼロになれば終了 */
+	bra    loop /* d1がゼロでなければループ */
 
 
 end_of_program:
