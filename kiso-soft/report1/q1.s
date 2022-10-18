2ef45e4848378e8a126564a37d22b3dd06f0f2d9
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2022-10-18 20:52:30 +0900

    [add]report1感想以外

--- kiso-soft/report1/q1.s
+++ kiso-soft/report1/q1.s
@@ -5,13 +5,14 @@ start:
 	move.w	(%a0)+, (%a1)+
 	move.w  #0x07F7, MASK
 	move.w  MASK, %d0
-	MOVE.L %A2,BOTTOM(%A0) 	
+	move.b  %A2,BOTTOM(%A0)
+	move.w	BOTTOM, %d1
 
 .section .data
 	.equ TOP, 0xFFFC00
 	.equ MASK, TOP+0x80
 	.dc.b   'a','b','c','d','e',0
-	.equ BOTTOM,        4
+	.equ BOTTOM,        50
 
 
 .end
