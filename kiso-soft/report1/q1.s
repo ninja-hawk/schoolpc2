52484d3763a7d5e488a4a5c3631ecf06548e3d42
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2022-10-18 17:02:33 +0900

    [update]token

--- kiso-soft/report1/q1.s
+++ kiso-soft/report1/q1.s
@@ -4,11 +4,15 @@
 start:
 	move.w	(%a0)+, (%a1)+
 	move.w  #0x07F7, MASK
+	move.w  MASK, %d0
+	MOVE.L %A2,BOTTOM(%A0) 	
 
 .section .data
 	.equ TOP, 0xFFFC00
 	.equ MASK, TOP+0x80
-		
+	.dc.b   'a','b','c','d','e',0
+	.equ BOTTOM,        4
+
 
 .end
 
