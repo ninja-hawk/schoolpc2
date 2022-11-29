118b9596f8950a5abfa85c64213e42f5ba74206e
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2022-11-29 16:48:52 +0900

    [update]in-out2

--- SW2/mtk/outchr.s
+++ SW2/mtk/outchr.s
@@ -1,25 +1,39 @@
-.extern 
 .global outbyte
+.equ SYSCALL_NUM_PUTSTRING,     2
 
-.section .text
+.text
 .even
-*******************************
-** outbyte
-** 引数：
-**       char型のデータ(スタックに格納済)
-** 出力：
-**       シリアルポート0に出力
-*******************************
+
 outbyte:
-		movem.l %d0-%d3, -(%sp)
-out_byte_loop:	
-		move.l #2, %d0       | PUTSTRINGを指定
-		move.l #0, %d1       | ch = 0
-		move.l ()
-		move.l #1, %d3       | size = 1
-		trap   #0
-		cmpi   #1, %d0       | PUTSTRINGの返り値が送信数と一致するか確認
-		bne    out_byte_loop | 一致しなければ再送信
-end_outbyte:	
-		movem.l (%sp)+, %d0-%d3
-		rts
+	movem.l %d0-%d3/%a1, -(%sp)
+outbyte_step0:
+
+	**引数のchを取る
+	lea.l (%sp), %a1
+	move.l #24, %d2	/*4xレジスタ5個+PC分4*/
+	add.l %d2, %a1
+	move.l (%a1), ch
+
+	**引数の文字を取る
+	move.l #7, %d2
+	add.l %d2, %a1
+	move.b (%a1), buf
+	
+	move.l #SYSCALL_NUM_PUTSTRING,%D0
+	move.l ch,    %d1        | ch = ch
+	move.l #buf,  %d2        | p  = #BUF
+	move.l #1,    %d3        | size = 1
+	trap #0
+
+	cmpi.l #1, %d0
+	bne outbyte_step0
+	movem.l (%sp)+, %d0-%d3/%a1
+	rts
+	
+	.section .bss
+buf:
+	.ds.b 1
+	.even
+ch:
+	.ds.l 1
+	.even	/*ここのevenは必要*/
