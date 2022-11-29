040ecd54f90af59d60b1ae372a5ed1a9df19fc97
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2022-11-29 16:39:33 +0900

    [add]in-outchr.s

--- SW2/mtk/inchrw.s
+++ SW2/mtk/inchrw.s
@@ -4,3 +4,18 @@
 .even
 
 inbyte:
+		movem.l %D0-%D3, -(%sp)
+inbyte_loop:
+		move.l #SYSCALL_NUM_GETSTRING, %D0
+		move.l #0,   %D1        | ch   = 0
+		move.l #In_BUF, %D2        | p    = #BUF
+		move.l #1, %D3        | size = 1
+		trap   #0
+        	cmpi   #1, %d0       | GETSTRINGの返り値が1と一致するか確認
+		bne    inbyte_loop | 一致しなければ再受信
+		movem.l	(%sp)+, %D0-%D3
+        	rts
+
+.section .bss
+In_BUF: 
+		.ds.b 1
--- SW2/mtk/outchr.s
+++ SW2/mtk/outchr.s
@@ -1,6 +1,25 @@
+.extern 
 .global outbyte
 
-.text
+.section .text
 .even
-
+*******************************
+** outbyte
+** 引数：
+**       char型のデータ(スタックに格納済)
+** 出力：
+**       シリアルポート0に出力
+*******************************
 outbyte:
+		movem.l %d0-%d3, -(%sp)
+out_byte_loop:	
+		move.l #2, %d0       | PUTSTRINGを指定
+		move.l #0, %d1       | ch = 0
+		move.l ()
+		move.l #1, %d3       | size = 1
+		trap   #0
+		cmpi   #1, %d0       | PUTSTRINGの返り値が送信数と一致するか確認
+		bne    out_byte_loop | 一致しなければ再送信
+end_outbyte:	
+		movem.l (%sp)+, %d0-%d3
+		rts
