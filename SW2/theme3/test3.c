338e9e30632df397b2135cb876bc11c65cd26725
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2023-01-12 18:02:36 +0900

    [update]theme3 LINE like chatting tools

--- SW2/theme3/test3.c
+++ SW2/theme3/test3.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include "mtk_c.h"
-
-
 FILE* com0in;
 FILE* com0out;
 FILE* com1in;
@@ -14,70 +12,56 @@ void init_file(){
    com1out = fdopen(4, "w");
 }
 
-void task1(){
-	char c[200];
-	P(0);
-	fprintf(com0out, "画面0から入力");
-	fscanf(com0in, "%s", c);
-	V(0);
-	while(1){
-		fprintf(com0out, "画面0だぜぇ");
-		fprintf(com1out, "画面1だぜぇ");
-    	}
-}
-
-void task2(){
-	//P(0);
-    	// フォントを緑色に
-    	fprintf(com0out, "\x1b[32m");
-    	fprintf(com1out, "\x1b[32m");
-    	//char d[200];
-	fprintf(com1out, "画面1だぜぇ　入力: ");
-	//fscanf(com1in, "%s", d);
-	//fprintf(com1out, "出力: %s\n", d);
-	//fprintf(com0out, "出力: %s\n", d);
-    while(1){
-	fprintf(com1out, "画面1だぜぇ");
-    }
-}
-
-void task3(){
+void task0(){
 while(1){
+    //P(0);
+    char s0;
+    //fprintf(com0out, "\n%s\n","------------------------------------画面0から入力：------------------------------------");
+    fscanf(com0in, "%s", &s0);
     P(0);
+    fprintf(com0out, "\e[A\e[K");
     // フォントを赤色に
     fprintf(com0out, "\x1b[31m");
     fprintf(com1out, "\x1b[31m");
-    char s;
-    fprintf(com0out, "画面0から入力：\n");
-    fscanf(com0in, "%s", &s);
-    fprintf(com0out, "画面0からの出力： %s\n", &s);
-    fprintf(com1out, "画面0からの出力： %s\n", &s);
+    fprintf(com0out, "\n%80s\n", &s0);
+    fprintf(com1out, "\e[A\e[K");
+    fprintf(com1out, "\n%s\n", &s0);
+    fprintf(com0out, "\x1b[0m");
+    fprintf(com1out, "\x1b[0m");
     V(0);
+    //V(0);
 }
 }
 
-void task4(){
+void task1(){
 while(1){
+    //P(0);
+    char s1;
+    //fprintf(com1out, "\n%s\n","------------------------------------画面1から入力：------------------------------------");
+    fscanf(com1in, "%s", &s1);
     P(0);
+    fprintf(com1out, "\e[A\e[K");
     // フォントを緑色に
     fprintf(com0out, "\x1b[32m");
     fprintf(com1out, "\x1b[32m");
-    char s;
-    fprintf(com1out, "画面1から入力：\n");
-    fscanf(com1in, "%s", &s);
-    fprintf(com0out, "画面1からの出力： %s\n", &s);
-    fprintf(com1out, "画面1からの出力： %s\n", &s);
+    fprintf(com0out, "\e[A\e[K");
+    fprintf(com0out, "\n%s\n", &s1);
+    fprintf(com1out, "\n%80s\n", &s1);
+    fprintf(com0out, "\x1b[0m");
+    fprintf(com1out, "\x1b[0m");
     V(0);
+    //V(0);
 }
 }
+
+
 
 int main(void){
   init_kernel();
   init_file();
-  //set_task(task1);
-  //set_task(task2);
-  set_task(task3);
-  set_task(task4);
+
+  set_task(task0);
+  set_task(task1);
   begin_sch();  
   return 0;
 }
